fd8aa793cc0bfa5b9b13bd2a94fc223d6bd71ab6
Kavya Kumar <tellkavya@gmail.com>
2019-09-22 12:45:15 -0700

    Add styling

--- src/App.js
+++ src/App.js
@@ -42,9 +42,9 @@ class App extends React.Component {
     if (err !== '') { 
       event.target.value = null
       return(
-        < div class="alert alert-warning alert-dismissible fade show" role = "alert" >
+        < div className="alert alert-info" role = "alert" >
           <strong>Holy guacamole!</strong> You should check in on some of those fields below.
-          < button type = "button" class="close" data-dismiss="alert" aria-label="Close" >
+          < button type = "button" className="close" data-dismiss="alert" aria-label="Close" >
                 <span aria-hidden="true">&times;</span>
           </button >
         </div >
